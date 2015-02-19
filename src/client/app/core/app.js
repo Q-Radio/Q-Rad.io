028e4e9918ea44acd20a70cfcdbeecc5f4a5c559
Eric <eric.n.benson@gmail.com>
2015-02-18 16:18:01 -0800

    (test) implements front-end, back-end, and integration testing framework

--- src/client/app/core/app.js
+++ src/client/app/core/app.js
@@ -1,23 +1,32 @@
 var songs = [];
 
 
-// var getSong = function(){		
-// 	$.ajax({
-// 		type: 'POST',
-// 		url: '/song',
-// 		data: JSON.stringify(songs),
-// 		dataType: 'json',
-// 		contentType: 'application/json',
-// 		success: function(data){
-// 			songs.push([data.title,data.artist_name,data.score]);
-// 			console.log(data.tracks.foreign_id);
-// 			// $(player).addSpotifyInfoToPlaylist([data.track.foreign_id]);
-// 			return data.tracks.foreign_id;
-// 		}
-// 	});
-// };
+var getSong = function(){		
+	$.ajax({
+		type: 'POST',
+		url: '/song',
+		data: JSON.stringify(songs),
+		dataType: 'json',
+		contentType: 'application/json',
+		success: function(data){
+			songs.push([data.title,data.artist_name,data.score]);
+			console.log(data.tracks.foreign_id);
+			// $(player).addSpotifyInfoToPlaylist([data.track.foreign_id]);
+			return data.tracks.foreign_id;
+		}
+	});
+};
 
-var playSong = function(){
-
-
+var getRandomSong = function(){
+	$.ajax({
+		type: 'POST',
+		url: '/random',
+		data: JSON.stringify(songs),
+		dataType: 'json',
+		contentType: 'application/json',
+		success: function(data){
+			songs.push([data.title,data.artist_name,data.score]);
+			console.log(data.tracks.foreign_id);
+		}
+	});
 };
