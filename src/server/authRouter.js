5e08e6c3593ab472f519ef14ffeeb9f41e3e1f2d
zdlopez <zachary.d.lopez@gmail.com>
2015-02-24 09:42:56 -0800

    (feat) adds authorization with spotify to server routes (refactor) splits server and authorization routes

---
+++ src/server/authRouter.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var auth = require('./auth.js');
+var requestHandler = require('./requestHandler.js');
+var Path = require('path');
+
+var authRouter = express.Router();
+
+var build = Path.resolve(__dirname,'../../build');
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login');
+}
+
+authRouter.get('/', ensureAuthenticated,
+  function(req, res) {
+    res.sendFile(build + '/index.html');
+});
+
+authRouter.get('/training-worker.js', ensureAuthenticated, requestHandler.trainingWorker);
+
+authRouter.post('/10songs',ensureAuthenticated, requestHandler.get10Songs);
+
+authRouter.post('/3songs',ensureAuthenticated, requestHandler.get3Songs);
+
+authRouter.post('/song', ensureAuthenticated,requestHandler.getRelatedSong);
+
+authRouter.post('/random', ensureAuthenticated, requestHandler.getRandomSong);
+
+authRouter.get('/auth/spotify',
+  auth.authenticate('spotify', {scope: 'user-read-private'}),
+  function(req, res){
+// The request will be redirected to spotify for authentication, so this
+// function will not be called.
+});
+
+authRouter.get('/login', function(req, res){
+  res.render('login.html', { user: req.user });
+});
+
+authRouter.get('/callback',
+  auth.authenticate('spotify', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+authRouter.use(express.static(build));
+
+authRouter.get('/logout', function(req, res){
+  console.log('I am logging out');
+  req.logout();
+  res.redirect('/');
+});
+
+module.exports = authRouter;
--- src/server/server.js
+++ src/server/server.js
@@ -3,36 +3,39 @@ var express  = require('express');
 var reloader = require('connect-livereload');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+
+var swig = require('swig');
+var consolidate = require('consolidate');
 var Path = require('path');
-var requestHandler = require('./requestHandler.js');
+var auth = require('./auth.js');
+var authRouter = require('./authRouter.js');
 
 var mongo = process.env.mongo || require('../../.ENV').mongo;
 var port = process.env.PORT || 8000;
+var host = process.env.HOST || 'localhost'
 
 var app = express();
+var build = Path.resolve(__dirname,'../../build');
+
+app.engine('html', consolidate.swig);
+app.set('views', build + '/templates/core/views');
+app.set('view engine', 'html');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(session({ secret: 'supermusic' }));
+app.use(auth.initialize());
+app.use(auth.session());
+
 app.use(reloader());
-var mypath = Path.resolve(__dirname,'../../build');
-console.log('path is ', mypath);
-app.use(express.static(mypath));
 
 mongoose.connect(mongo);
 
-app.get('/training-worker.js',requestHandler.trainingWorker);
-
-
-app.post('/10songs',requestHandler.get10Songs);
-
-app.post('/3songs',requestHandler.get3Songs);
-
-
-app.post('/random',requestHandler.getRandomSong);
-
 var port = process.env.PORT || 8000;
 
+app.use(authRouter);
 
 app.listen(port, function() {
   console.log('listening on port ', port);
